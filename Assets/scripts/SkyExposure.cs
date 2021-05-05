4b025ddddebb877d38e68f379f3de90497516187
hamirashkan <aa.haghshenas@gmail.com>
2021-05-05 11:13:38 +0200

    Sky Exposure Update
    1- Some changes in "SkyExposure" script
    2- Add "AddObject" script

--- Assets/scripts/SkyExposure.cs
+++ Assets/scripts/SkyExposure.cs
@@ -6,12 +6,13 @@ public class SkyExposure : MonoBehaviour
 {
     public int Width;
     public int Length;
-    private int[,] matrix = new int[25  , 45];
     int count;
     int countRay;
     float Exposure;
     public static float Exposure_text;
-    public Text Skytext;
+    public Vector3[] posArray = new Vector3[10];
+    public float[] percentArray = new float[10];
+    //public Text Skytext;
     private bool isClicked = false;
     public Color X1 { get; private set; }
     public Color X2 { get; private set; }
@@ -19,15 +20,25 @@ public class SkyExposure : MonoBehaviour
     void Start()
     {
 
-        setText();
         X1 = Color.cyan;
         X2 = Color.red;
 
     }
+    private void Cal_posiotion()
+    {
+
+        for(int i=0; i< posArray.Length; i++)
+        {
+            count = 0;
+            Exposure = 0;
+            transform.position = posArray[i];
+            RayCast();
+            percentArray[i] = (((float)countRay - (float)count) / (float)countRay) * 100;
+        }
+    }
 
     private void RayCast()
     {
-
         countRay = 0;
         RaycastHit hit;
         for (int i = 0; i < Width * 2 - 1; i += 1)
@@ -40,7 +51,7 @@ public class SkyExposure : MonoBehaviour
 
                 if (Physics.Raycast(transform.position, Direction, out hit, 45f))
                 {
-                    Debug.DrawRay(transform.position,Direction * 45, X2);
+                    Debug.DrawRay(transform.position,Direction * 5, X2);
                     count += 1;
                     
                 }
@@ -57,40 +68,45 @@ public class SkyExposure : MonoBehaviour
         myButtonStyle1.fontSize = 15;
         Rect myRect1 = new Rect(10, 350, 270, 40);
         GUI.Box(myRect1, "Sky exposure: " + SkyExposure.Exposure_text.ToString("0.0") + "%", myButtonStyle);
+
+        if (GUI.Button(new Rect(280, 350, 40, 40), "Ray", myButtonStyle1))
+        {
+            
+            if (isClicked)
+            {
+                
+                isClicked = false;
+                X1 = Color.clear;
+                X2 = Color.clear;
+            }
+            else
+            {
+
+                isClicked = true;
+                X1 = Color.cyan;
+                X2 = Color.red;
+            }
+        }
     }
 
-    //     if (GUI.Button(new Rect(280, 350, 40, 40), "Ray", myButtonStyle1))
-    //     {
-
-    //         if (isClicked)
-    //         {
-    //             isClicked = false;
-    //             X1 = Color.clear;
-    //             X2 = Color.white;
-
-    //         }
-    //         else
-    //         {
-    //             isClicked = true;
-    //             X1 = Color.cyan;
-    //             X2 = Color.red;
-    //         }
-    //     }
-    // }
 
 
     void Update  ()
     {
-
+        
         count = 0;
         Exposure = 0;
-        RayCast();
-        Exposure = ((13969f - count) / 13969f) * 100; // Write an equation for calculating the number of rays casts.
-        setText();
+        if (isClicked)
+        {
+            RayCast();
+            Cal_posiotion();
+        }
+        Exposure = (((float)countRay - (float)count) / (float)countRay) * 100; // Write an equation for calculating the number of rays casts.
+        //setText();
         Exposure_text = Exposure;
     }
-    void setText()
+    /*void setText()
     {
             Skytext.text = ("SkyExposure : " + Exposure.ToString("0.00") + "%");    
-    }
+    }*/
  }
