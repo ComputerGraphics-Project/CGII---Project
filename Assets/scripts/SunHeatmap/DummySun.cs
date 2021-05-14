abd23d47b2d55b0423ff5dec3a273a6577d7c366
Amirabbas Hojjati <amirhojjati.lp@gmail.com>
2021-05-14 14:10:32 +0200

    Save/Load fix

--- Assets/scripts/SunHeatmap/DummySun.cs
+++ Assets/scripts/SunHeatmap/DummySun.cs
@@ -146,7 +146,7 @@ public class DummySun : MonoBehaviour
     {
         //save
         List<string> linesToWrite = new List<string>();
-        if (ShadowHM0 != null)
+        if (ShadowHM0 != null && ShadowHM0.Length != 0)
         {
             StringBuilder size = new StringBuilder();
             StringBuilder startX = new StringBuilder();
@@ -195,54 +195,60 @@ public class DummySun : MonoBehaviour
         //load
 	//check if the path exists?????????
         string path = Application.persistentDataPath + "/data.json";
-        string fileData = System.IO.File.ReadAllText(path);
-        string[] lines = fileData.Split("\n"[0]);
-        int size = int.Parse(lines[0]);
-        float startX = float.Parse(lines[1]);
-        //float startY = float.Parse(lines[2]);
-        float startZ = float.Parse(lines[3]);
-        int[,] tempHM = new int[size + 1, size + 1];
-
-        for (int i = 0; i <= size; i++)
+        if (System.IO.File.Exists(path))
         {
-            for (int j = 0; j <= size; j++)
-            {
-                tempHM[i,j] = int.Parse(lines[i+4].Split(","[0])[j]);
-            }
-        }
+            string fileData = System.IO.File.ReadAllText(path);
+            string[] lines = fileData.Split("\n"[0]);
+            int size = int.Parse(lines[0]);
+            float startX = float.Parse(lines[1]);
+            //float startY = float.Parse(lines[2]);
+            float startZ = float.Parse(lines[3]);
+            int[,] tempHM = new int[size + 1, size + 1];
 
-        GameObject tempPoint;
-        float intensity;
-        float Max = tempHM.Cast<int>().Max();
-                
-        for (int i = 0; i <= size; i++)
-        {
-            for (int j = 0; j <= size; j++)
+            for (int i = 0; i <= size; i++)
             {
-                //terrain height
-                RaycastHit hit3;
-                Vector3 RayOrigin0 = new Vector3(startX + i, 1000, startZ + j);
-                Vector3 RayDir0 = Vector3.down;
-                int layerMask = 1 << 8;
-                if (Physics.Raycast(RayOrigin0, RayDir0, out hit3, Mathf.Infinity, layerMask))
+                for (int j = 0; j <= size; j++)
                 {
-                    tempPoint = Instantiate(hmPoint);
-                    tempPoint.transform.SetParent(hmObj);
-                    tempPoint.transform.position = new Vector3(startX + i, hit3.point.y, startZ + j);
-                    if (Max != 0)
+                    tempHM[i, j] = int.Parse(lines[i + 4].Split(","[0])[j]);
+                }
+            }
+
+            GameObject tempPoint;
+            float intensity;
+            float Max = tempHM.Cast<int>().Max();
+
+            for (int i = 0; i <= size; i++)
+            {
+                for (int j = 0; j <= size; j++)
+                {
+                    //terrain height
+                    RaycastHit hit3;
+                    Vector3 RayOrigin0 = new Vector3(startX + i, 1000, startZ + j);
+                    Vector3 RayDir0 = Vector3.down;
+                    int layerMask = 1 << 8;
+                    if (Physics.Raycast(RayOrigin0, RayDir0, out hit3, Mathf.Infinity, layerMask))
                     {
-                        intensity = tempHM[j, i] / Max;
+                        tempPoint = Instantiate(hmPoint);
+                        tempPoint.transform.SetParent(hmObj);
+                        tempPoint.transform.position = new Vector3(startX + i, hit3.point.y, startZ + j);
+                        if (Max != 0)
+                        {
+                            intensity = tempHM[j, i] / Max;
+                        }
+                        else
+                        {
+                            intensity = 0;
+                        }
+                        tempPoint.GetComponent<Renderer>().material.color = new Color(1 - intensity, 0, 1 - intensity, 1);
                     }
-                    else
-                    {
-                        intensity = 0;
-                    }
-                    tempPoint.GetComponent<Renderer>().material.color = new Color(1 - intensity, 0, 1 - intensity, 1);
                 }
             }
         }
 
-
+        else
+        {
+            Debug.Log("File Does Not Exist!");
+        }
 
 
 
