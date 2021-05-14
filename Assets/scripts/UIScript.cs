647571f13dafcf6c75cf1cfd089def8fffaa0d85
Ravitej Bhagavathi <bhagavathi.ravitej@gmail.com>
2021-05-14 13:55:51 +0200

    SaveLoadMenu
    Added FileBox prefab to Scene. It has the file option for save-load

--- Assets/scripts/UIScript.cs
+++ Assets/scripts/UIScript.cs
@@ -7,35 +7,104 @@ using System;
 
 public class UIScript : MonoBehaviour
 {
-    //UI elements - SimParamPanel
-    public TMP_InputField TMP_IF;
-
+    ////////////////UI elements/////////////////////
     
+    //SimParamPanel
+    //UI elements - SimParamPanel
+    
+
+    //////Data plugin panel//////////
+
+    public Button Save;
+    public Button Load;
+
+    //menu system 
+    public GameObject fileBox;
+    public TMP_Text option1;
+    public TMP_Text option2;
+    public TMP_Text option3;
+    public TMP_Text option4;
+
+    //temp variables
+    private bool fileBoxActive;
+
+    //interface variables
+    [NonSerialized] public string ToFileName;  //file name where the data will be saved to
+    [NonSerialized] public string FromFileName; //file name where the data will be retrieved from
+    
+    /////////////////////////////////
+
+    //Simulation parameters panel////
+
     //OK and Cancel buttons
     public Button SimParamOKButton;
     public Button SimParamCancelButton;
 
 
+    //User Inputs
+    public TMP_InputField TMP_IF;
+
     //interface variables
     [NonSerialized] public float heatmapSize;
+
+    ////////////////////////////////////
 
     // Start is called before the first frame update
     void Start()
     {
+        fileBox.SetActive(false);
+        fileBoxActive = false;
         
     }
 
     // Update is called once per frame
     void Update()
     {
+
+    }
+
+
+
+    //Data plugin
+
+    //save
+    public void onSave()
+    {
+        //show/hide the file box
+        fileBox.SetActive(!fileBoxActive);
+
+        //toggle state
+        fileBoxActive = !fileBoxActive;    
+    }
+
+    public void onLoad()
+    {
         
     }
 
-    public void SimParamOK()
+
+    public void option1OnClick()
     {
-        heatmapSize = float.Parse(TMP_IF.text);
-        //Debug.Log(heatmapSize);
-        //return heatmapSize;
+        ToFileName = option1.text;
+      
+    }
+
+    public void option2OnClick()
+    {
+        ToFileName = option2.text;
+     
+    }
+
+    public void option3OnClick()
+    {
+        ToFileName = option3.text;
+        
+    }
+
+    public void option4OnClick()
+    {
+        ToFileName = option4.text;
+      
     }
 
 
