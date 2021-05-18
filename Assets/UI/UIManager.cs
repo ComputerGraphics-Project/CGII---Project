13b1800bc4538fe3e8ae32c12e817a3c201eacb2
Peder Blomvik <36536013+pederblom@users.noreply.github.com>
2021-05-18 18:07:38 +0200

    UIManager program switch
    UIManager now can switch between programs

--- Assets/UI/UIManager.cs
+++ Assets/UI/UIManager.cs
@@ -6,16 +6,23 @@ using UnityEngine.UI;
 public class UIManager : MonoBehaviour
 {
     // Can change this to get the different images in the script without public prefabs like this
+    public GameObject StartWindow;
     public GameObject BusIndicatorUI;
+    public GameObject BusIndicator;
     public GameObject SkyExposureUI;
+    public GameObject SkyExposure;
     public GameObject LandmarkUI;
+    public GameObject Landmark;
     public GameObject ShadowHeatmapUI;
+    public GameObject ShadowHeatmap;
     public TMPro.TMP_Dropdown UIdropdown;
     private GameObject currentUI;
+    private GameObject currentProgram;
     // Start is called before the first frame update
     void Start()
     {
         currentUI = new GameObject();
+        currentProgram = new GameObject();
         UIdropdown.onValueChanged.AddListener(delegate
         {
             changeUI(UIdropdown.value);
@@ -54,35 +61,74 @@ public class UIManager : MonoBehaviour
         return uiImage;
     }
 
+    GameObject getProgram(int ui)
+    {
+        var program = currentProgram;
+
+        switch (ui)
+        {
+            case 0:
+                currentProgram = null;
+                break;
+            case 1:
+                program = SkyExposure;
+                break;
+            case 2:
+                program = ShadowHeatmap;
+                break;
+            case 3:
+                program = Landmark;
+                break;
+            case 4:
+                program = BusIndicator;
+                break;
+        }
+        return program;
+    }
+
     void hideAll()
     {
         BusIndicatorUI.SetActive(false);
         SkyExposureUI.SetActive(false);
         ShadowHeatmapUI.SetActive(false);
         LandmarkUI.SetActive(false);
+        BusIndicator.SetActive(false);
+        SkyExposure.SetActive(false);
+        ShadowHeatmap.SetActive(false);
+        Landmark.SetActive(false);
     }
 
-    void toggleActiveUI(GameObject ui, bool b)
+    void toggleActive(GameObject g, bool b)
     {
-
-        for (int i = 0; i < ui.transform.childCount; i++)
+        if (g)
         {
-            var child = ui.transform.GetChild(i).gameObject;
-            if (child) child.SetActive(b);
+            for (int i = 0; i < g.transform.childCount; i++)
+            {
+                var child = g.transform.GetChild(i).gameObject;
+                if (child) child.SetActive(b);
+            }
+            g.gameObject.SetActive(b);
         }
-        ui.gameObject.SetActive(b);
-        if (b) currentUI = ui;
+
     }
 
     void changeUI(int ui)
     {
         var newUI = getUI(ui);
-        if (!newUI) hideAll();
+        var newProgram = getProgram(ui);
+        if (!newUI || !newProgram) hideAll();
         else
         {
-            toggleActiveUI(currentUI, false);
-            toggleActiveUI(newUI, true);
+            toggleActive(currentUI, false);
+            toggleActive(newUI, true);
+            currentUI = newUI;
+            toggleActive(currentProgram, false);
+            toggleActive(newProgram, true);
+            currentProgram = newProgram;
         }
-
+        if (ui == 0)
+            StartWindow.SetActive(true);
+        else
+            StartWindow.SetActive(false);
     }
 }
